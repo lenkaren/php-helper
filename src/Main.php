f83029ce333a3b6e2f4d1af63c8c8bd16df3db77
Aren <lenk.legend@outlook.com>
2018-04-11 21:04:30 +0800

    first commit

---
+++ src/Main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aren
+ * Date: 2018/4/11
+ * Time: 下午8:56
+ */
+
+namespace Lenkaren\PHPHelper;
+
+class Main
+{
+    public static function hello()
+    {
+        HelloWorld();
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aren
+ * Date: 2018/4/11
+ * Time: 下午8:44
+ */
+
+if (!function_exists('HelloWorld'))
+{
+    function HelloWorld()
+    {
+        echo 'Hello World, My name is PHP Helper.' . PHP_EOL;
+    }
+}
